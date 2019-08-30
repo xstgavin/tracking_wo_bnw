52e833e7ef70c6a56b5ea7b06724211030daef61
i-xiaoshengtao <i-xiaoshengtao@360.cn>
2019-08-30 15:23:48 +0800

    modidy utils.py to support non-bbox at first frame

--- mylabel/utils.py
+++ mylabel/utils.py
@@ -38,13 +38,17 @@ def draw_track_path(frame,bbox,tid):
     pt1 = (bbox[0]+bbox[2],bbox[1]+bbox[3])
     print(color[str(tid)])
     cv2.rectangle(frame,pt0,pt1,color[str(tid)],2)
-    cv2.putText(frame,'%03d'%tid,pt0,cv2.FONT_HERSHEY_SIMPLEX,0.5,color[str(tid+1)],2)
+    cv2.putText(frame,'%03d'%tid,pt0,cv2.FONT_HERSHEY_SIMPLEX,0.2,color[str(tid+1)],1)
     return frame
 
 def set_bbox(window_name,frame,idx_str, tid,jdat):
     
     nw_box = cv2.selectROI(window_name, frame, False, False)
     is_set = False
+    if idx_str not in jdat.keys():
+        jdat[idx_str] = []
+        b_data = [tid,nw_box[0],nw_box[1],nw_box[2],nw_box[3]]
+        jdat[idx_str].append(b_data)
     for bbox in jdat[idx_str]:
         if  bbox[0] != tid:
             continue
@@ -58,7 +62,7 @@ def set_bbox(window_name,frame,idx_str, tid,jdat):
         jdat[idx_str].append([tid,nw_box[0],nw_box[1],nw_box[2],nw_box[3]])
     
 
-def get_labels(path, extension='pysot_crt',fmt='old'):
+def get_labels(path, extension='pysot',fmt='old'):
     label_list = glob.glob(path+'*.'+extension)
     for label_name in label_list:
         print(label_name)
@@ -77,6 +81,22 @@ def get_labels(path, extension='pysot_crt',fmt='old'):
             framebk = frame.copy()
             frameIdx = frameIdx +1
             idx_str = '%06d'%frameIdx
+            if idx_str not in jdat.keys():
+                cv2.imshow(window_name,frame)
+                cv2.waitKey(30)
+                x=input('change?')
+                if x=='':
+                    continue
+                else:
+                    tid = int(x)
+                    set_bbox(window_name,frame,idx_str,tid,jdat)
+                    for bbinfo in jdat[idx_str]:
+                        tid = bbinfo[0]
+                        bbox=np.array([bbinfo[1],bbinfo[2],bbinfo[3],bbinfo[4]])
+                        frame=draw_track_path(frame,bbox,tid)
+                    cv2.imshow(window_name,frame)
+                    cv2.waitKey(30)
+                    continue
             for bbinfo in jdat[idx_str]:
                 tid = bbinfo[0]
                 bbox=np.array([bbinfo[1],bbinfo[2],bbinfo[3],bbinfo[4]])
@@ -101,7 +121,8 @@ def get_labels(path, extension='pysot_crt',fmt='old'):
         fid.close()
     
 def test():
-    get_labels('../../data_real/videos/',fmt='new')
+    #get_labels('../../data_real/videos/',extension='pysot_crt',fmt='new')
+    get_labels('../../data_real/videos/',extension='pysot',fmt='new')
 
 if __name__ == "__main__":
     test()
